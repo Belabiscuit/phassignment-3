03d070132caad353b58e0b88f1db09eb3dcda394
Mohammad Tohfaye Samdani <sifatmuhammad282@gmail.com>
2026-06-15 02:47:58 +0600

    values inserted in all tables

--- QUERY.sql
+++ QUERY.sql
@@ -32,3 +32,97 @@ create table bookings (
   payment_status varchar(50) check (payment_status in ('Pending','Confirmed','Cancelled','Refunded')),
   total_cost decimal(10,2) not null check(total_cost >= 0)
 );
+
+insert into
+  users (user_id, full_name, email, role, phone_number)
+values
+  (
+    1,
+    'Tanvir Rahman',
+    'tanvir@mail.com',
+    'Football Fan',
+    '+8801711111111'
+  ),
+  (
+    2,
+    'Asif Haque',
+    'asif@mail.com',
+    'Football Fan',
+    '+8801722222222'
+  ),
+  (
+    3,
+    'Sajjad Rahman',
+    'sajjad@mail.com',
+    'Ticket Manager',
+    '+8801733333333'
+  ),
+  (
+    4,
+    'Jannat Ara',
+    'jannat@mail.com',
+    'Football Fan',
+    NULL
+  );
+
+insert into
+  matches (
+    match_id,
+    fixture,
+    tournament_category,
+    base_ticket_price,
+    match_status
+  )
+values
+  (
+    101,
+    'Real Madrid vs Barcelona',
+    'Champions League',
+    150,
+    'Available'
+  ),
+  (
+    102,
+    'Man City vs Liverpool',
+    'Premier League',
+    120,
+    'Selling Fast'
+  ),
+  (
+    103,
+    'Bayern Munich vs PSG',
+    'Champions League',
+    130,
+    'Available'
+  ),
+  (
+    104,
+    'AC Milan vs Inter Milan',
+    'Serie A',
+    90,
+    'Sold Out'
+  ),
+  (
+    105,
+    'Juventus vs Roma',
+    'Serie A',
+    80,
+    'Available'
+  );
+
+insert into
+  bookings (
+    booking_id,
+    user_id,
+    match_id,
+    seat_number,
+    payment_status,
+    total_cost
+  )
+values
+  (501, 1, 101, 'A-12', 'Confirmed', 150),
+  (502, 1, 102, 'B-04', 'Confirmed', 120),
+  (503, 2, 101, 'A-13', 'Confirmed', 150),
+  (504, 2, 101, NULL, NULL, 150),
+  (505, 3, 102, 'C-20', 'Pending',120);
+
